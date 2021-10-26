770b745438a334fed26ef3234d3c2152f71da7a7
Serhiy-Mysyshyn <serhiymysyshyn@gmail.com>
2021-10-26 19:20:09 +0300

    Lab4 Ex1 Step 3

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
 
 from datetime import datetime
 import os
@@ -6,6 +8,11 @@ import sys
 import platform
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SerhiyMysyshyn'
+
+class LoginForm(FlaskForm):
+    username = StringField('username')
+    password = PasswordField('password')
 
 def getFooterData():
     OS_info = os.name + " " + platform.system() + " " + platform.release()
@@ -48,6 +55,13 @@ def about():
 @app.route('/myworks/')
 def myworks():
     return render_template('myworks.html', data=getFooterData(), projectsData=projectsData)
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    form = LoginForm
+    if form.validate_on_submit():
+        return '<h1>The username is {}. The password is {}.'.format(form.username.data, form.password.data)
+    return render_template('form.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
