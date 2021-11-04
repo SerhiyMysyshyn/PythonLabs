f3e439039de2dd90c6c1c4bea3edcf3c18a58f17
Serhiy-Mysyshyn <serhiymysyshyn@gmail.com>
2021-11-04 23:12:35 +0200

    Lab7 Done

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 import json
-from . import app
-from flask import render_template, url_for, flash, redirect, session, request
-from .forms import LoginForm, RegisterForm
-# from app.models import User, Post
+from . import app, db
+from flask import render_template, url_for, flash, redirect, session, abort
+from .forms import RegisterDoc, RegisterForm, LoginForm
+from .models import User
 from app.getAdditionalData import *
 from app.getFunction import getFooterData, write_to_json, validation
 
@@ -10,40 +10,19 @@ from app.getFunction import getFooterData, write_to_json, validation
 def home():
     return render_template('home.html', data=getFooterData())
 
-
 @app.route('/about/')
 def about():
     return render_template('about.html', data=getFooterData(), skills=skills)
-
 
 @app.route('/myworks/')
 def myworks():
     return render_template('myworks.html', data=getFooterData(), projectsData=projectsData)
 
-
-# LAB 4 ---------------------------------------------------------------------------------------------------------------------------------------------------------
-@app.route('/form', methods=['POST', 'GET'])
-def form():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if request.method == 'POST':
-            if len(request.form['username']) > 5:
-                flash('Форма надіслана успішно!', category='success')
-                usernameData = form.username.data
-                passwordData = form.password.data
-                return render_template('form.html', form=form, usernameData=usernameData, passwordData=passwordData,
-                                       data=getFooterData())
-            else:
-                flash('Форма не відправлена! Рекомендація: постарайтесь придумати довший логін!', category='error')
-                return render_template('form.html', form=form, data=getFooterData())
-
-    return render_template('form.html', form=form, data=getFooterData())
-
-
 # LAB 5 ---------------------------------------------------------------------------------------------------------------------------------------------------------
 @app.route('/registerForm', methods=['GET', 'POST'])
-def registerForm():
-    form = RegisterForm()
+def formRegisterDoc():
+    form = RegisterDoc()
+
     validation(form)
 
     if form.validate_on_submit():
@@ -52,22 +31,61 @@ def registerForm():
             flash('Форма надіслана успішно! Дані успішно записані у json файл!', category='success')
         else:
             flash('Error', category='error')
-        return redirect(url_for('registerForm'))
+        return redirect(url_for('formRegisterDoc'))
 
     try:
         sesiya = session['email']
+        with open('MysyshynDB.json') as f:
+            data_files = json.load(f)
+        form.email.data = sesiya
+        form.e_number.data = data_files[sesiya]['e_number']
+        form.e_pin.data = data_files[sesiya]['e_pin']
+        form.e_year.data = data_files[sesiya]['e_year']
+        form.d_series.data = data_files[sesiya]['d_series']
+        form.d_number.data = data_files[sesiya]['d_number']
     except:
         return render_template("registerForm.html", form=form, data=getFooterData())
 
-    with open('MysyshynDB.json') as f:
-        data_files = json.load(f)
+    return render_template("registerForm.html", form=form, data=getFooterData())
 
-    return render_template("registerForm.html",
-                           form=form,
-                           email=sesiya,
-                           e_number=data_files[sesiya]['e_number'],
-                           e_pin=data_files[sesiya]['e_pin'],
-                           e_year=data_files[sesiya]['e_year'],
-                           d_series=data_files[sesiya]['d_series'],
-                           d_number=data_files[sesiya]['d_number'],
-                           data=getFooterData())
+
+# LAB 7 ---------------------------------------------------------------------------------------------------------------------------------------------------------
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, password=form.password1.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Ви успішно зареєстрували акаунт: { form.username.data } !', category='success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form_reg=form, data=getFooterData())
+
+
+@app.route("/logIn", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.verify_password(form.password.data):
+            flash(f'Ви ввійшли як користувач: {user.email}!', category='success')
+            return redirect(url_for('login'))
+        else:
+            flash('Невірний пароль або логін!', category='error')
+            return redirect(url_for('login'))
+
+    return render_template('logIn.html', form=form, data=getFooterData())
+
+
+@app.route("/users", methods=['GET', 'POST'])
+def users():
+    all_users = User.query.all()
+    count = User.query.count()
+    if count == 0:
+        abort(404)
+    return render_template('userList.html', all_users=all_users, count=count, data=getFooterData())
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html', data=getFooterData())
--- config.py
+++ config.py
@@ -1,2 +1,8 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 SECRET_KEY = 'SerhiyMysyshyn'
 WTF_CSRF_ENABLED = True
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'mysyshynlabs.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
