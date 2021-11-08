cf446f57b9656c1b65cebd489f3aa656b190b846
Serhiy-Mysyshyn <serhiymysyshyn@gmail.com>
2021-11-08 20:50:37 +0200

    Lab 9 Done

--- app/models.py
+++ app/models.py
@@ -1,16 +1,19 @@
-from . import db, bcrypt
+from . import db, bcrypt, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def user_loader(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    user_country = db.Column(db.String(3))
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
 
-    def __init__(self, username, user_country, email, password, image_file='default.png'):
+    def __init__(self, username, email, password, image_file='default.png'):
         self.username = username
-        self.user_country = user_country
         self.email = email
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
         self.image_file = image_file
