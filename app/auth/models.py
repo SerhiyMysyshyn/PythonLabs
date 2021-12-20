b1a9a57f1064038e6d96284cd0c07728a62686c6
Serhiy-Mysyshyn <serhiymysyshyn@gmail.com>
2021-12-21 01:14:03 +0200

    lab14 done

--- app/auth/models.py
+++ app/auth/models.py
@@ -30,18 +30,3 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.password}')"
-
-class PostType(enum.Enum):
-    News = 'News'
-    Publication = 'Publication'
-    Other = 'Other'
-
-class Posts(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50), nullable=False)
-    text = db.Column(db.Text, nullable=True)
-    image_file = db.Column(db.String(20), nullable=False, default='postdefault.jpg')
-    created = db.Column(db.DateTime, default=db.func.now())
-    type = db.Column(db.Enum(PostType))
-    enabled = db.Column(db.Boolean, default=True, nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
